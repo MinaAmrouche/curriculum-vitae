1e299bd9e253da86b88f5f8c3e131fcbfcb75874
Mina Amrouche <mina.amrouche9@gmail.com>
2019-09-04 17:37:27 +0200

    Change some styles

--- app/src/components/Introduction/Introduction.js
+++ app/src/components/Introduction/Introduction.js
@@ -8,7 +8,7 @@ const Introduction = () => {
   return (
     <div className="Introduction">
       <Separator />
-      <div className="Introduction__column">
+      <div className="Introduction__name">
         <img src={ProfilePicture} alt="" className="introduction__picture" />
         <h1>MINA AMROUCHE</h1>
         <h3>Front-end web developer</h3>
@@ -19,6 +19,16 @@ const Introduction = () => {
         <ContactItem picto="icon-phone" content="+33 6 10 73 56 53" />
         <ContactItem picto="icon-github" content="@minaamrouche" />
       </div>
+      <Separator />
+      <h2>Profile</h2>
+      <Separator />
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <Separator />
+      <h2>Technical skills</h2>
+      <Separator />
     </div>
   );
 };
